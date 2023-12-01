97db475021d49192e9545670a5e3a96bf495a582
Aarav <43528744+Aarav87@users.noreply.github.com>
2023-12-01 18:28:55 -0500

    updated the footer padding

--- components/footer.js
+++ components/footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
         <footer className="bg-black text-white p-10 lg:p-20 font-karla font-semibold text-md">
             {/* Footer items */}
             <div className="grid grid-cols-12 mb-20">
-                <Image src={Logo} width={124} height={64} className="col-span-12 lg:col-span-2 sm:mb-5" />
+                <Image src={Logo} width={124} height={64} className="col-span-12 lg:col-span-2 pb-10" />
                 <div className="grid grid-cols-2 lg:grid-cols-4 col-span-12 lg:col-span-10 gap-y-10 gap-x-20 lg:gap-x-5">
                     <div className="space-y-4">
                         <h1 className="mb-7">About</h1>
