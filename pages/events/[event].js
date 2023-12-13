9c8377e60ec8d34e8576c5b001c09d7209b774b4
Aarav <43528744+Aarav87@users.noreply.github.com>
2023-12-12 23:50:37 -0500

    attempt to fix a bug on events page

--- pages/events/[event].js
+++ pages/events/[event].js
@@ -119,7 +119,7 @@ export async function getStaticProps(context) {
         status: response.results[0].properties.Status.status.name,
         date: response.results[0].properties.Date.date.start,
         coverImg: response.results[0].properties.Cover.url,
-        signup: response.results[0].properties.Signup.url || "null",
+        signup: response.results[0].properties.Signup.url || null,
         video: response.results[0].properties.Video.url || null,
         description: response.results[0].properties.Description.rich_text[0].plain_text,
         presenters: response.results[0].properties.Presenters.rich_text[0].plain_text || null,
