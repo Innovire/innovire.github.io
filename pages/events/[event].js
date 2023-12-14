278771a3d7781c46b18692b6ca099d7574d1a4a3
Aarav <43528744+Aarav87@users.noreply.github.com>
2023-12-13 22:34:01 -0500

    fixed a bug on events page

--- pages/events/[event].js
+++ pages/events/[event].js
@@ -21,40 +21,44 @@ export default function Event({ event }) {
 
                 {/* COVER */}
                 <CldImage
-                    src={`${event.path}/cover`}
+                    src={`${event?.path}/cover`}
                     height="2000"
                     width="2000"
                     className="h-[50vh] w-full rounded-lg object-cover object-bottom"
                 />
 
                 {/* NAME */}
-                <h1 className="text-5xl lg:text-7xl font-bold">
-                    {event.name}
-                </h1>
+                {event?.name ? (
+                    <h1 className="text-5xl lg:text-7xl font-bold">
+                        {event.name}
+                    </h1>
+                ): null}
 
                 {/* STATUS AND DATE */}
-                <div className="flex-row space-x-5">
-                    <span className={`${event.status === "Future" ? "text-green-600 bg-green-100" : "text-blue-600 bg-blue-100"} py-1 px-3 text-md rounded-md`}>
-                        {event.status}
-                    </span>
-                    <span className="text-purple-600 bg-purple-100 py-1 px-3 text-md rounded-md">
-                        {event.date}
-                    </span>
-                </div>
+                {event?.status && event?.date ? (
+                    <div className="flex-row space-x-5">
+                        <span className={`${event.status === "Future" ? "text-green-600 bg-green-100" : "text-blue-600 bg-blue-100"} py-1 px-3 text-md rounded-md`}>
+                            {event.status}
+                        </span>
+                            <span className="text-purple-600 bg-purple-100 py-1 px-3 text-md rounded-md">
+                            {event.date}
+                        </span>
+                    </div>
+                ): null}
 
                 <div className="font-karla font-semibold leading-loose text-pg text-gray-500 space-y-5">
                     {/* DESCRIPTION */}
-                    <p>{event.description}</p>
+                    {event?.description ? <p>{event.description}</p> : null}
 
                     {/* PRESENTERS */}
-                    {event.presenters ? <p>{event.presenters}</p> : null}
+                    {event?.presenters ? <p>{event.presenters}</p> : null}
 
                     {/* MENTORS */}
-                    {event.mentors ? <p>{event.mentors}</p> : null}
+                    {event?.mentors ? <p>{event.mentors}</p> : null}
                 </div>
 
                 {/* IMAGES */}
-                {event.status === "Past" ? (
+                {event?.status === "Past" ? (
                     <div className="flex flex-auto md:h-[50vh] gap-3">
                         {images.map((image) => (
                             <CldImage
@@ -69,7 +73,7 @@ export default function Event({ event }) {
                 ) : null}
 
                 {/*{ VIDEO }*/}
-                {event.video ? <iframe className="rounded-lg w-full lg:w-[50%] aspect-video" src={event.video} /> : null}
+                {event?.video ? <iframe className="rounded-lg w-full lg:w-[50%] aspect-video" src={event.video} /> : null}
             </div>
         </Layout>
     )
@@ -98,8 +102,8 @@ export async function getStaticPaths() {
 }
 
 // Get all the event details required to render the page
-export async function getStaticProps(context) {
-    const eventID = context.params.event
+export async function getStaticProps({ params }) {
+    const eventID = params.event;
 
     // Get the event using the eventID
     const response = await notion.databases.query({
@@ -114,15 +118,15 @@ export async function getStaticProps(context) {
 
     // Add the necessary information to eventDetails
     const eventDetails = {
-        name: response.results[0].properties.Name?.title[0].text.content || null,
-        path: response.results[0].properties.Path?.rich_text[0].plain_text || null,
-        status: response.results[0].properties.Status?.status.name || null,
-        date: response.results[0].properties.Date?.date.start || null,
-        signup: response.results[0].properties.Signup?.url || null,
-        video: response.results[0].properties.Video?.url || null,
-        description: response.results[0].properties.Description?.rich_text[0].plain_text || null,
-        presenters: response.results[0].properties.Presenters?.rich_text[0].plain_text || null,
-        mentors: response.results[0].properties.Mentors?.rich_text[0].plain_text || null
+        name: response.results[0].properties.Name.title[0].text.content,
+        path: response.results[0].properties.Path.rich_text[0].plain_text,
+        status: response.results[0].properties.Status.status.name,
+        date: response.results[0].properties.Date.date.start,
+        signup: response.results[0].properties.Signup?.url,
+        video: response.results[0].properties.Video?.url,
+        description: response.results[0].properties.Description.rich_text[0].plain_text,
+        presenters: response.results[0].properties.Presenters.rich_text[0]?.plain_text || null,
+        mentors: response.results[0].properties.Mentors.rich_text[0]?.plain_text || null
     };
 
     return {
